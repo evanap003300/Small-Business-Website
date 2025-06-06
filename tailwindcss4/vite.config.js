43cb3b92296e37bc10ad6c67fbe3497a72fec702
Evan Phillips <evanap003300@gmail.com>
2025-06-05 20:52:48 -0400

    Updated code

--- tailwindcss4/vite.config.js
+++ tailwindcss4/vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import eslint from 'vite-plugin-eslint'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+            eslint({
+      // This disables linting during build
+      failOnWarning: false,
+      failOnError: false,
+      emitWarning: false,
+      emitError: false,
+      include: [],
+      exclude: ['node_modules']
+                  })],
   base: process.env.VITE_BASE_PATH || "/Small-Business-Website",
 });
