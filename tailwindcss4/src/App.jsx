90a26d7f7dc1701a3cef64177c127c293b2d438b
Evan Phillips <evanap003300@gmail.com>
2025-06-14 18:44:55 -0400

    Importing code

--- tailwindcss4/src/App.jsx
+++ tailwindcss4/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ServiceWebsite from './components/ServiceWebsite';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return <ServiceWebsite />;
+  <Analytics />
 }
 
 export default App;
